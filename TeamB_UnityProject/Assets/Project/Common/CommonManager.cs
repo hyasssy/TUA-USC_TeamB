8ae241b1331a5ed3f40fb375a2a67c2b28d0c77d
Yuto Hayashi <hyasssy@gmail.com>
2021-02-10 10:17:58 +0900

    PlayMakerインストール、初期セットアップ作業

---
+++ TeamB_UnityProject/Assets/Project/Common/CommonManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Singleton object. Check if any instance already exist in Awake and if yes, destroy itself automaticcaly.
+public class CommonManager : SingletonMonoBehaviour<CommonManager> {
+    //ボタンを長押ししたらシーン遷移ボタンプレファブを生成、表示・非表示
+    public void DisplaySceneManager () {
+        // FindObjectOfType<> ();
+    }
+}
