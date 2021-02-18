6017988fe7ef8f98fc31fcc5a9b79dd77a76682f
Yuto Hayashi <hyasssy@gmail.com>
2021-02-18 11:33:02 +0900

    room構築中

---
+++ TeamB_UnityProject/Assets/Project/s2_Room/Scripts/S2Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S2Manager : MonoBehaviour
+{
+    //イニシャライズする。
+    //CommonManagerのStateを参照し、必要なところまで飛ぶようにする。
+    //Sceneロードした時に初期化される想定。
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
