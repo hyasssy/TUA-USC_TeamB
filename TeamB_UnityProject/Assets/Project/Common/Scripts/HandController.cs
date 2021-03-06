33a1582c13335c15ac664e22effc14ac4feb4523
Yuto Hayashi <hyasssy@gmail.com>
2021-03-06 15:50:22 +0900

    フォント、s1_roomの簡単な作成

---
+++ TeamB_UnityProject/Assets/Project/Common/Scripts/HandController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    Texture2D cursorImage = default;
+    [SerializeField]
+    int handSize = 150;
+
+    //これ安定しないから、ずっとシーンにオブジェおいて、マウスに追従させた方がいいかも。
+    private void Awake() {
+        Cursor.visible = false;
+        TextureScale.Bilinear(cursorImage, handSize, handSize);
+        var pointfingerPos = new Vector2(handSize / 5, handSize / 7);
+        Cursor.SetCursor(cursorImage, pointfingerPos, CursorMode.Auto);
+    }
+    //hoverしたら画像差し替え
+    //クリックしたら画像差し替え
+}
