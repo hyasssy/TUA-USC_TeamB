cdf1600fb4c095a9db8a315ba5f288645e2c8a9a
Kaito Fujinuma <sokonashi0124@gmail.com>
2021-03-11 21:59:02 +0900

    エラー多発のためブランチを切り直した。

---
+++ TeamB_UnityProject/Assets/Project/Fujinuma/s_Dog/Scripts/DogCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogCollider : MonoBehaviour
+{
+    bool isStroke = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void StrokeOn()
+    {
+        isStroke = true;
+    }
+    void StrokeOff()
+    {
+        isStroke = false;
+    }
+}
---
+++ TeamB_UnityProject/Assets/Project/Fujinuma/s_Dog/Scripts/HandObjectManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandObjectManager : MonoBehaviour
+{
+    float _z;
+    private void Start()
+    {
+        _z = gameObject.transform.position.z;
+    }
+    // 位置座標
+    private Vector3 position;
+    // スクリーン座標をワールド座標に変換した位置座標
+    private Vector3 screenToWorldPointPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3でマウス位置座標を取得する
+        position = Input.mousePosition;
+        position.x -= Screen.width;
+        position.y -= Screen.height;
+        position.z = _z;
+
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(-position);
+        Debug.Log(screenToWorldPointPosition);
+        screenToWorldPointPosition.z = -_z;
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position = -screenToWorldPointPosition;
+    }
+}
