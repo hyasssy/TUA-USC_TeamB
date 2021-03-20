ec0ee3c2fab2b67718340078a560d1b4833019ad
Yuto Hayashi <hyasssy@gmail.com>
2021-03-21 01:17:16 +0900

    追従してくる手とあたり判定スクリプト作成、不要ファイル削除、cinemachine導入

--- TeamB_UnityProject/Assets/Project/Common/Scripts/HandController.cs
+++ TeamB_UnityProject/Assets/Project/Common/Scripts/HandController.cs
@@ -1,21 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
 public class HandController : MonoBehaviour
 {
     [SerializeField]
-    Texture2D cursorImage = default;
+    Transform hand = default;
     [SerializeField]
-    int handSize = 150;
-
-    //これ安定しないから、ずっとシーンにオブジェおいて、マウスに追従させた方がいいかも。
-    private void Awake() {
-        Cursor.visible = false;
-        TextureScale.Bilinear(cursorImage, handSize, handSize);
-        var pointfingerPos = new Vector2(handSize / 5, handSize / 7);
-        Cursor.SetCursor(cursorImage, pointfingerPos, CursorMode.Auto);
+    RectTransform canvasRect = default;
+    [SerializeField]
+    Vector2 targetCursorPos = new Vector2(30f, -80f);
+    Vector3 _mousePos;
+    public bool isClickable = true;
+    private void Start() {
+        this.UpdateAsObservable()
+        .Subscribe(_ =>
+        {
+            CheckHover();
+            Click();
+            TracingHand();
+        });
     }
     //hoverしたら画像差し替え
     //クリックしたら画像差し替え
+
+    void CheckHover(){
+        if(!isClickable) return;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//手から飛ばすのも一考。
+        RaycastHit hit;
+        if (Physics.Raycast(ray,out hit,10.0f))
+        {
+            var coll = hit.collider;
+            Debug.Log(coll.gameObject.transform.name);
+            var touchable = coll.gameObject.GetComponent<ITouchable>();
+            if(touchable != null){
+                Debug.Log("クリックできるよ");
+                hand.localScale = Vector3.one * 1.2f;//簡易的に大きくする
+            }else{
+                hand.localScale = Vector3.one;//簡易的に戻す
+            }
+        }
+    }
+    void Click(){
+        if(!isClickable) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10.0f))
+            {
+                var coll = hit.collider;
+                Debug.Log(coll.gameObject.transform.name);
+                var touchable = coll.gameObject.GetComponent<ITouchable>();
+                if (touchable != null)
+                {
+                    touchable.Clicked();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+    void TracingHand(){
+        _mousePos = Input.mousePosition;
+        var magnification = canvasRect.sizeDelta.x / Screen.width;
+        _mousePos.x = _mousePos.x * magnification - canvasRect.sizeDelta.x / 2 + targetCursorPos.x;
+        _mousePos.y = _mousePos.y * magnification - canvasRect.sizeDelta.y / 2 + targetCursorPos.y;
+        _mousePos.z = transform.localPosition.z;
+
+        hand.localPosition = Vector3.Lerp(hand.localPosition, _mousePos, Time.deltaTime);
+    }
 }
---
+++ TeamB_UnityProject/Assets/Project/s_Room/Scripts/RoomObjTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomObjTest : MonoBehaviour, ITouchable
+{
+    public void Clicked(){
+        Debug.Log("クリックされた");
+    }
+}
